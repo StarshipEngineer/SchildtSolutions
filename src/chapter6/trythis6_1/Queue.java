69b301f222bf9326526dc27bbaf594bdc3444907
Dan Shuman <dan.13.shuman@gmail.com>
2015-08-28 19:49:07 -0500

    Organized directory structure

---
+++ src/chapter6/trythis6_1/Queue.java
@@ -0,0 +1,31 @@
+/* Try This 6-1
+An improved Queue class for characters
+ */
+
+class Queue {
+    private char q[];
+    private int putloc, getloc;
+
+    Queue(int size) {
+        q = new char[size];
+        putloc = getloc = 0;
+    }
+
+    void put(char ch) {
+        if(putloc == q.length) {
+            System.out.println(" - Queue is full.");
+            return;
+        }
+
+        q[putloc++] = ch;
+    }
+
+     char get() {
+         if (getloc == putloc) {
+             System.out.println(" - Queue is empty.");
+             return (char) 0;
+         }
+
+         return q[getloc++];
+     }
+}
